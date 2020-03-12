7aa9c0afa9670303c080f61fdbd7c9081617710c
tanxuheng <729558575@qq.com>
2020-03-12 15:34:05 +0800

    first push

--- app/src/main/java/com/txh/treeviw/TreeViewActivity.java
+++ app/src/main/java/com/txh/treeviw/TreeViewActivity.java
@@ -10,7 +10,7 @@ public class TreeViewActivity extends AppCompatActivity  {
 
 
     private View reset;
-    private TreeView treeView;
+    private TreeView<String> treeView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,16 +21,16 @@ public class TreeViewActivity extends AppCompatActivity  {
 
         reset.setOnClickListener(v -> treeView.reset());
 
-        NodeModel tree = new NodeModel("根节点");
+        NodeModel<String> tree = new NodeModel<>("根节点");
 
-        NodeModel model1 = new NodeModel("孩子1");
-        model1.addChild(new NodeModel("1"));
-        NodeModel modelll = new NodeModel("2");
+        NodeModel<String> model1 = new NodeModel<>("孩子1");
+        model1.addChild(new NodeModel<>("1"));
+        NodeModel<String> modelll = new NodeModel<>("2");
 
-        NodeModel child3 = new NodeModel("1");
-        child3.addChild(new NodeModel("1"));
+        NodeModel<String> child3 = new NodeModel<>("1");
+        child3.addChild(new NodeModel<>("1"));
 
-        NodeModel child4 = new NodeModel("2");
+        NodeModel<String> child4 = new NodeModel<>("2");
 //        child4.addChild(new NodeModel("1"));
 //        child4.addChild(new NodeModel("2"));
 //        child4.addChild(new NodeModel("3"));
@@ -38,23 +38,23 @@ public class TreeViewActivity extends AppCompatActivity  {
         child3.addChild(child4);
 
         modelll.addChild(child3);
-        modelll.addChild(new NodeModel("2"));
+        modelll.addChild(new NodeModel<>("2"));
         model1.addChild(modelll);
-        model1.addChild(new NodeModel("3"));
-        NodeModel model2 = new NodeModel("孩子2");
-        model2.addChild(new NodeModel("1"));
-        model2.addChild(new NodeModel("2"));
-        NodeModel model3 = new NodeModel("孩子3");
-        model3.addChild(new NodeModel("1"));
-        model3.addChild(new NodeModel("2"));
-        NodeModel model4 = new NodeModel("孩子4");
-        NodeModel model5 = new NodeModel("孩子5");
-        model5.addChild(new NodeModel("1"));
-        NodeModel model6 = new NodeModel("孩子6");
-        model6.addChild(new NodeModel("1"));
-        model6.addChild(new NodeModel("2"));
-        model6.addChild(new NodeModel("3"));
-        model6.addChild(new NodeModel("4"));
+        model1.addChild(new NodeModel<>("3"));
+        NodeModel<String> model2 = new NodeModel<>("孩子2");
+        model2.addChild(new NodeModel<>("1"));
+        model2.addChild(new NodeModel<>("2"));
+        NodeModel<String> model3 = new NodeModel<>("孩子3");
+        model3.addChild(new NodeModel<>("1"));
+        model3.addChild(new NodeModel<>("2"));
+        NodeModel<String> model4 = new NodeModel<>("孩子4");
+        NodeModel<String> model5 = new NodeModel<>("孩子5");
+        model5.addChild(new NodeModel<>("1"));
+        NodeModel<String> model6 = new NodeModel<>("孩子6");
+        model6.addChild(new NodeModel<>("1"));
+        model6.addChild(new NodeModel<>("2"));
+        model6.addChild(new NodeModel<>("3"));
+        model6.addChild(new NodeModel<>("4"));
         tree.addChild(model1);
         tree.addChild(model2);
         tree.addChild(model3);
